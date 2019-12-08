40304b0b42ed578bc20a3163d83a86a569903206
henev <hristo.peychev.enev@gmail.com>
2019-12-08 16:00:20 +0200

    Initial commit

---
+++ Components.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const One = () => <h1>Content of page one</h1>;
+const Two = () => <h1>Content of page two</h1>;
+const Three = () => <h1>Content of page three</h1>;
+const Four = () => <h1>Content of page four</h1>;
+
+export { One, Two, Three, Four };
--- index.js
+++ index.js
@@ -1,26 +1,46 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+import { One, Two, Three, Four } from './Components';
+import { LayoutOne, LayoutTwo } from './Layouts';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <h3>Content of main App component</h3>
+      <ul>
+        <li><Link to="/one">One</Link></li>
+        <li><Link to="/two">Two</Link></li>
+        <li><Link to="/three">Three</Link></li>
+        <li><Link to="/four">Four</Link></li>
+      </ul>
 
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
-    );
-  }
+      <Switch>
+        <RouteWrapper path="/one" component={One} layout={LayoutOne} />
+        <RouteWrapper path="/two" component={Two} layout={LayoutOne} />
+        <RouteWrapper path="/three" component={Three} layout={LayoutTwo} />
+        <RouteWrapper path="/four" component={Four} layout={LayoutTwo} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+function RouteWrapper({
+  component: Component, 
+  layout: Layout, 
+  ...rest
+}) {
+  return (
+    <Route {...rest} render={(props) =>
+      <Layout {...props}>
+        <Component {...props} />
+      </Layout>
+    } />
+  );
+}
+
+function RouteWrapperTwo() {
+
 }
 
 render(<App />, document.getElementById('root'));
